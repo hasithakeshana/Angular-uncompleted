573d765fd684aaf163f4d1c8e9ea8f2521ddd765
hasitha <hasithakeshana9900@gmail.com>
2020-04-13 13:45:48 +0530

    'initial'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-app';
+//  faCoffee = faCoffee;
+public picker = new Date();
+
+date = new Date();
+
+
+  todos = [];
+
+  addTodo(newTodoLabel,time){
+    var newTodo = {
+      label : newTodoLabel,
+      priority : 1,
+      done : false,
+      date : this.date,
+      time : time
+    }
+
+
+    this.todos.push(newTodo);
+    console.log(this.todos);
+  }
+
+  deleteTodo(todo){
+      this.todos = this.todos.filter(t => t.label !== todo.label);
+  }
+
+  getDate(date)
+  {
+    console.log(date);
+    var x = date.toISOString();
+
+    console.log('xxxxxxxxxxxxxxxxxxxxxxxxx',x);
+
+  }
+
+  events: string[] = [];
+
+  addEvent(type: string, event) {
+    this.events.push(`${type}: ${event.value}`);
+
+    console.log(event.value);
+
+    this.date = event.value;
+
+    console.log(this.date);
+  }
+
+
 }
